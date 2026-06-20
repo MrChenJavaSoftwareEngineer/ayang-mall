db72bb4efdf705627212529d6edc03f4656dba86
ayang <3648901811@qq.com>
2026-06-20 13:51:03 +0800

    2.0v

---
+++ ayang-admin/src/test/java/com/ayang/ayangadmin/AyangAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayang.ayangadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyangAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
